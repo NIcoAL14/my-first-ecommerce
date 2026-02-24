1fdc115805420ae1086fde17ad24ec3574f0c364
NIcoAL14 <nicoalexanderlyncharenas@gmail.com>
2026-02-23 20:33:44 -0500

    feat: fetch and display products in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import ProductCard from './components/ProductCard'
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(res => res.json())
+      .then(data => {
+        setProducts(data)
+      })
+  }, [])
+
   return (
     <>
+      {products.map(product => (
+        <ProductCard
+          key={product.id} // importante para evitar warnings en React
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          imageUrl={product.images?.[0]} // la API devuelve un array de imágenes
+        />
+      ))}
+
       <ProductCard
-        title="Zapatillas Runing"
+        title="Zapatillas Running"
         price="120"
         description="Las mejores zapatillas"
         imageUrl="https://picsum.photos/600/400"
       />
+
       <ProductCard
         title="Reloj de Tiempo"
         price="200"
