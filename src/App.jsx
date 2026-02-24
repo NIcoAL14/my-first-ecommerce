bdadd0bbf1ef51e60fd3f77647bee9b8d2287cb5
NIcoAL14 <nicoalexanderlyncharenas@gmail.com>
2026-02-24 10:26:15 -0500

    feat: add Prettier configuration and refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -18,12 +18,7 @@ function App() {
         imageUrl="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
-      <ProductCard
-        title="Mouse RGB"
-        price="100"
-       description="El mejor mouse RGB"
-      />
-
+      <ProductCard title="Mouse RGB" price="100" description="El mejor mouse RGB" />
     </>
   )
 }
