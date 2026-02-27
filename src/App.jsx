075ad88248312969d1aaab278e47f804c2a0ff2a
NIcoAL14 <nicoalexanderlyncharenas@gmail.com>
2026-02-26 21:11:11 -0500

    feat: refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,40 @@ import './App.css'
 import ProductCard from './components/ProductCard'
 
 function App() {
+  const productsData = [
+    {
+      title: 'Zapatillas Runing',
+      price: 120,
+      description: 'Las mejores zapatillas',
+      imageUrl:
+        'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      title: 'Reloj de Tiempo',
+      price: 200,
+      description: 'El mejor reloj de Tiempo',
+      imageUrl:
+        'https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      title: 'Mouse RGB',
+      price: 100,
+      description: 'El mejor mouse RGB',
+    },
+  ]
+
   return (
-    <>
-      <ProductCard
-        title="Zapatillas Runing"
-        price="120"
-        description="Las mejores zapatillas"
-        imageUrl="https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      />
-
-      <ProductCard
-        title="Reloj de Tiempo"
-        price="200"
-        description="El mejor reloj de Tiempo"
-        imageUrl="https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-      />
-
-      <ProductCard title="Mouse RGB" price="100" description="El mejor mouse RGB" />
-    </>
+    <div className="card-list">
+      {productsData.map((product, index) => (
+        <ProductCard
+          key={index}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          imageUrl={product.imageUrl}
+        />
+      ))}
+    </div>
   )
 }
-
 export default App
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,27 +1,21 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import './ProductCard.css'
 
-function ProductCard({title, price, description, imageUrl= 'https://picsum.photos/600/400'}) {
-
+function ProductCard({ title, price, description, imageUrl = 'https://picsum.photos/600/400' }) {
   const [isAdded, setIsAdded] = useState('false')
   console.log('isAdded', isAdded)
 
   return (
     /* Create Product Card HTML */
     <div id="card-01" className="card">
-      <img 
-        src={imageUrl}
-        alt={title}
-        className="card-image"
-      />
+      <img src={imageUrl} alt={title} className="card-image" />
       <div className="card-content">
         <h3 className="card-title">{title}</h3>
-        <p className="card-text">
-          {description}
-        </p>
+        <p className="card-text">{description}</p>
         <div className="card-footer">
           <span className="card-price">${price}</span>
-          <button className={`card-button ${isAdded ? 'added' : ''}`}
+          <button
+            className={`card-button ${isAdded ? 'added' : ''}`}
             onClick={() => setIsAdded('isAdded' === 'true' ? 'false' : 'true')} /* !isAdded */
           >
             {isAdded === 'true' ? 'Agregado' : 'Agregar'}
@@ -29,7 +23,7 @@ function ProductCard({title, price, description, imageUrl= 'https://picsum.photo
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default ProductCard;
+export default ProductCard
