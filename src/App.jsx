c343210d2ca710bd1956551aa20d98181e2c8930
NIcoAL14 <nicoalexanderlyncharenas@gmail.com>
2026-03-04 13:55:50 -0500

    feat: add routing and new pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,21 @@
-import { useState } from 'react'
-import './App.css'
-import PromoBanner from './components/PromoBanner'
-import Header from './components/Header'
-import ProductList from './components/ProductList'
-import Container from './components/Container'
-import { productsAdapter } from './adapters/products.adapter'
-import Loader from './components/Loader'
-import useFetch from './hooks/useFetch'
-import ErrorMessage from './components/ErrorMessage'
-import { API_ENDPOINTS } from './utils/constants'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import AddProductPage from './pages/AddProductPage'
+import CartPage from './pages/CartPage'
 
 function App() {
-  const [showPromo, setShowPromo] = useState(true)
-
-  const {
-    data: productsRawData,
-    isLoading: isLoadingProducts,
-    error: errorProducts,
-  } = useFetch(API_ENDPOINTS.PRODUCTS_AVAILABLES)
-
-  const {
-    data: suggestedRawData,
-    isLoading: isLoadingSuggested,
-    error: errorSuggested,
-  } = useFetch(API_ENDPOINTS.PRODUCTS_SUGGESTED)
-
-  const {
-    data: snacksRawData,
-    isLoading: isLoadingSnacks,
-    error: errorSnacks,
-  } = useFetch(API_ENDPOINTS.PRODUCTS_SNACKS)
-
-  const productsData = productsRawData ? productsRawData.map(productsAdapter) : []
-  const productsSuggestedData = suggestedRawData ? suggestedRawData.map(productsAdapter) : []
-  const productsSnacksData = snacksRawData ? snacksRawData.map(productsAdapter) : []
-
-  const onClose = () => {
-    setShowPromo(false)
-  }
-
   return (
-    <>
-      <Header />
-      {showPromo && <PromoBanner onClose={onClose} />}
-
-      {errorProducts && <ErrorMessage message="No se pudo cargar los productos" />}
-      {errorSuggested && <ErrorMessage message="No se pudo cargar los productos sugeridos" />}
-      {errorSnacks && <ErrorMessage message="No se pudo cargar los snacks" />}
-
-      {!errorProducts && (
-        <Container title="Productos disponibles">
-          <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Estos son los productos que puedes comprar</p>
-          {isLoadingProducts ? <Loader /> : <ProductList productsData={productsData} />}
-        </Container>
-      )}
-
-      {!errorSuggested && (
-        <Container title="Productos sugeridos">
-          <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de productos</p>
-          {isLoadingSuggested ? <Loader /> : <ProductList productsData={productsSuggestedData} />}
-        </Container>
-      )}
-
-      {!errorSnacks && (
-        <Container title="Productos snacks">
-          <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de snacks</p>
-          {isLoadingSnacks ? <Loader /> : <ProductList productsData={productsSnacksData} />}
-        </Container>
-      )}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/add-product" element={<AddProductPage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   )
-
-  // Alternativa: renderizar HomePage directamente
-  return <HomePage />
 }
 
 export default App
