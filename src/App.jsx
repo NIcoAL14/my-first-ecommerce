6b581af138f56c9322a3f82e2d515e8a37842e52
NIcoAL14 <nicoalexanderlyncharenas@gmail.com>
2026-03-04 10:42:23 -0500

    feat: fetch and display products from API in App component update ProductCard styles for better text handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import PromoBanner from './components/PromoBanner'
 import Header from './components/Header'
@@ -6,58 +6,90 @@ import ProductList from './components/ProductList'
 import Container from './components/Container'
 
 function App() {
-  const [cartCount, setCartCount] = useState(0)
   const [showPromo, setShowPromo] = useState(true)
 
-  const productsData = [
-    {
-      id: '01',
-      title: 'Zapatillas Running',
-      price: 120,
-      description: 'Las mejores zapatillas',
-      imageUrl: 'https://images.unsplash.com/photo-1542291026-7eec264c27ff',
-    },
-    {
-      id: '02',
-      title: 'Reloj de Tiempo',
-      price: 200,
-      description: 'El mejor reloj de Tiempo',
-      imageUrl: 'https://images.unsplash.com/photo-1563861826100-9cb868fdbe1c',
-    },
-    {
-      id: '03',
-      title: 'Mouse RGB',
-      price: 100,
-      description: 'El mejor mouse RGB',
-    },
-  ]
+  const [productsData, setProductsData] = useState([])
+  const [productsSuggestedData, setProductsSuggestedData] = useState([])
+  const [productsSnacksData, setProductsSnacksData] = useState([])
+
+  useEffect(() => {
+    // Productos Disponibles
+    fetch('https://api.escuelajs.co/api/v1/products?offset=0&limit=6')
+      .then((response) => response.json())
+      .then((data) => {
+        const productsAdapteds = data.map((product) => {
+          const { id, title, price, description, images } = product
+          return {
+            id,
+            title,
+            price,
+            description,
+            imageUrl: images[0],
+          }
+        })
+        setProductsData(productsAdapteds)
+      })
+      .catch((error) => console.error(error))
+
+    // Productos Sugeridos
+    fetch('https://api.escuelajs.co/api/v1/products?offset=6&limit=2')
+      .then((response) => response.json())
+      .then((data) => {
+        const productsAdapteds = data.map((product) => {
+          const { id, title, price, description, images } = product
+          return {
+            id,
+            title,
+            price,
+            description,
+            imageUrl: images[0],
+          }
+        })
+        setProductsSuggestedData(productsAdapteds)
+      })
+      .catch((error) => console.error(error))
+
+    // Productos Snacks
+    fetch('https://api.escuelajs.co/api/v1/categories/19/products?offset=0&limit=6')
+      .then((response) => response.json())
+      .then((data) => {
+        const productsAdapteds = data.map((product) => {
+          const { id, title, price, description, images } = product
+          return {
+            id,
+            title,
+            price,
+            description,
+            imageUrl: images[0],
+          }
+        })
+        setProductsSnacksData(productsAdapteds)
+      })
+      .catch((error) => console.error(error))
+  }, [])
 
   const onClose = () => {
     setShowPromo(false)
   }
 
-  const handleAddToCart = (isAdded) => {
-    if (isAdded === true) {
-      setCartCount((count) => count + 1)
-    }
-    if (isAdded === false) {
-      setCartCount((count) => count - 1)
-    }
-  }
-
   return (
     <>
-      <Header cartCount={cartCount} />
+      <Header />
       {showPromo && <PromoBanner onClose={onClose} />}
 
       <Container title="Productos disponibles">
         <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Estos son los productos que puedes comprar</p>
-        <ProductList productsData={productsData} handleAddToCart={handleAddToCart} />
+        <ProductList productsData={productsData} />
       </Container>
 
       <Container title="Productos sugeridos">
         <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de productos</p>
-        <ProductList productsData={productsData.slice(0, 2)} handleAddToCart={handleAddToCart} />
+        <ProductList productsData={productsSuggestedData} />
+      </Container>
+
+      <Container title="Productos snacks">
+        <p style={{ textAlign: 'center', marginBottom: '1rem' }}>Explora nuestra selección de snacks</p>
+        <ProductList productsData={productsSnacksData} />
       </Container>
     </>
   )
