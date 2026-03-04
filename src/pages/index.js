72cb99a8b0d6cbd65d32af1383c251128b6b14b3
NIcoAL14 <nicoalexanderlyncharenas@gmail.com>
2026-03-04 16:49:57 -0500

    feat: add lazy load for AboutPage, AddProductPage, and CartPage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
+import React from 'react'
 import HomePage from './HomePage'
 import AboutPage from './AboutPage'
 import AddProductPage from './AddProductPage'
 import CartPage from './CartPage'
 
 export { HomePage, AboutPage, AddProductPage, CartPage }
+export const AboutPage = React.lazy(() => import('./AboutPage'))
+export const AddProductPage = React.lazy(() => import('./AddProductPage'))
+export const CartPage = React.lazy(() => import('./CartPage'))
+
+export { HomePage }
